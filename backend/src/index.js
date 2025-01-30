94672f9b215ee668d6b3b59840fa5b22abda7abb
maulana-tech <firdaussyah03@gmail.com>
2025-01-30 16:53:55 +0700

    Add profile update and authentication check functionality; integrate Cloudinary for image uploads

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
