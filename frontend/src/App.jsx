bdc67261c70bb28cb4e89d2046952d8e307b3099
maulana-tech <firdaussyah03@gmail.com>
2025-01-30 23:11:35 +0700

    Add profile and settings pages; implement authentication store and navbar component; configure CORS and axios instance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import './index.css'
+import Navbar from './components/Navbar'
+
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
+import HomePage from './pages/HomePage';
+import { useAuthStore } from './store/userAuthStore';
+import { useEffect } from 'react';
+
+import { Loader } from "lucide-react";
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) return (
+
+    <div className = "flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
+
   return (
-    <div className="text-red-500 text-4xl">
-      Hello World
+    <div>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+      </Routes>
+
+      <Toaster />
+
     </div>
   )
 }
