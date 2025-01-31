881875b94bd315e65800c027085f4d2affaf7db2
maulana-tech <firdaussyah03@gmail.com>
2025-02-01 01:04:17 +0700

    coba

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["https://chat-app-6w3x.vercel.app/"],
     credentials: true,
 }
 ))
